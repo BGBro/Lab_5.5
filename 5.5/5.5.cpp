5aacc2a034263ca3381d5329c75706d6c29f52e9
BGBro <bohdan.horbachov.itis.2020@lpnu.ua>
2020-11-08 14:02:16 +0200

    master

---
+++ 5.5/5.5.cpp
@@ -0,0 +1,24 @@
+﻿#include<iostream>
+
+using namespace std;
+
+int a = 0;
+
+int C(int k, int n) {
+
+    a++;
+
+    if (k == n || k == 0) {
+        return 1;
+    }
+    return (C(k - 1, n - 1) + C(k, n - 1));
+}
+
+
+int main() {
+    cout << C(3, 7);
+    cout << "\n" << a;
+
+    cout << endl;
+    system("pause");
+}
---
+++ Test 5.5/Test 5.5.cpp
@@ -0,0 +1,20 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../5.5/5.5.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Test55
+{
+	TEST_CLASS(Test55)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			int t;
+			t = C(3, 7);
+			Assert::AreEqual(35, t);
+		}
+	};
+}
